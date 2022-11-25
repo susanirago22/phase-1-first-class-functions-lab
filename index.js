79ca2ec84f091cb3302a2ab68ba581591ff58e04
susanirago22 <susaniragoorani@gmail.com>
2022-11-25 23:55:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const drivers = [ 'Susan', 'Joy', 'Irene']
+const returnFirstTwoDrivers = (drivers) => 
+{
+    const newArray = drivers.slice(0, 2);
+    return (newArray);
+}
+const returnLastTwoDrivers = (drivers) =>
+{
+    const lastTwoElements = drivers.slice(-2);
+    return (lastTwoElements);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (num) =>
+{
+   return () => num * 5
+      
+}
+const fareDoubler = (fare) =>
+{
+    return fare * 2;
+}
+const fareTripler = (far) => 
+{
+    return far * 3;
+}
+const selectDifferentDrivers = (drivers,func) =>
+{
+   return func(drivers);
+}
